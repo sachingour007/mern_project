9a2e96f2fa0b3a28d0472ad6ff3c7a8710114a0d
sachingour <gour.sachin.sg@gmail.com>
2024-02-28 00:00:33 +0530

    contact us page development

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import aboutimg from "../asset/images/loginFromImg.png";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="aboutSection">
+      <div className="wrapper">
+        <div className="aboutInfo">
+          <h3>
+            Welcome, <span>Sachin Gour</span>
+          </h3>
+          <h2>Why Choose Us?</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            cum ratione illo, odio, obcaecati quod tenetur nemo doloribus
+            explicabo, molestias pariatur. Quisquam odio dignissimos
+            voluptatibus magni nemo laboriosam et odit ut. Ipsum illum
+            explicabo, quam quos aspernatur perferendis nostrum quis cupiditate
+            qui nesciunt magnam officia eveniet beatae non fuga eaque unde
+            numquam voluptate ex vero ratione porro sint. Dolor consequatur
+            placeat enim dignissimos amet quia perspiciatis commodi, accusamus
+            ducimus ipsam architecto animi ad, earum suscipit natus aperiam eius
+            aliquid! Perferendis et cupiditate veritatis voluptate officiis
+            saepe reprehenderit vero iste provident impedit facere nemo,
+            necessitatibus quas nesciunt, odio exercitationem rerum repudiandae.
+          </p>
+          <a href="">
+            <button>Contact Us</button>
+          </a>
+          <a href="">
+            <button>Learn More</button>
+          </a>
+        </div>
+        <div className="aboutImg">
+          <img src={aboutimg} alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [contactData, setContactData] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
 
-export default Contact
+  
+
+  return (
+    <section className="contectSection">
+      <div className="wrapper">
+        <h2>Contact Us</h2>
+        <div className="formDetails">
+          <form action="">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" />
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" name="email" />
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              cols="30"
+              rows="10"
+            ></textarea>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
