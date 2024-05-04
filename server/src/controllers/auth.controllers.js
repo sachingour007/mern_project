ec340f59b9d6e3b49e9e0d98449443ed9d7dbb15
sachingour <gour.sachin.sg@gmail.com>
2024-05-04 12:11:43 +0530

    changes in sever

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -60,6 +60,7 @@ const login = async (req, res) => {
     // const user = await bcrypt.compare(password, userExist.password);
 
     const user = await userExist.comparePassword(password);
+    console.log(user);
 
     if (user) {
       res.status(201).json({
